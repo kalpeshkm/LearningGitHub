2cc8711c74f456262b484ec9cc4848d2f7d8a64a
kalpeshkm <114247760+kalpeshkm@users.noreply.github.com>
2024-02-12 12:23:32 +0530

    Update DevClass.java

--- src/main/java/DevClass.java
+++ src/main/java/DevClass.java
@@ -1,6 +1,5 @@
 public class DevClass {
     public static void main(String[] args) {
-        System.out.println("DevClass + Stream 2");
-        // Checkin from Stream2
+        System.out.println("DevClass");
     }
 }
