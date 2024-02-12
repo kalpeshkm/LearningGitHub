959cf9e565edd867719e56d4066dd1bbcd30e204
bhaskarbasu1 <159751414+bhaskarbasu1@users.noreply.github.com>
2024-02-12 14:27:25 +0530

    added
    added

--- src/main/java/DevClass.java
+++ src/main/java/DevClass.java
@@ -1,6 +1,6 @@
 public class DevClass {
     public static void main(String[] args) {
-        System.out.println("DevClass + Stream2");
+        System.out.println("DevClass + Stream2 - new feature is added");
         // Checking from Stream2
     }
 }
